77e5490dfa42ccb9372cc51f8800393dab9b950e
Maddie02 <madlensarkisian@gmail.com>
2020-01-22 20:03:57 +0200

    Added buyer's id

--- model/ad.py
+++ model/ad.py
@@ -2,7 +2,7 @@ from database import SQLite
 
 class Ad(object):
 
-    def __init__(self, id, title, description, price, date, is_active, owner_id):
+    def __init__(self, id, title, description, price, date, is_active, owner_id, buyer_id=None):
         self.id = id
         self.title = title
         self.description = description
@@ -10,6 +10,7 @@ class Ad(object):
         self.date = date
         self.is_active = is_active
         self.owner_id = owner_id
+        self.buyer_id = buyer_id
 
     def to_dict(self):
         ad_data = self.__dict__
@@ -18,12 +19,12 @@ class Ad(object):
     def save(self):
         with SQLite() as db:
             if self.id == None:
-                values = (self.title, self.description, self.price, self.date, self.is_active, self.owner_id)
-                db.execute("INSERT INTO ad (title, description, price, date, is_active, owner_id) VALUES (?, ?, ?, ?, ?, ?)", values)
+                values = (self.title, self.description, self.price, self.date, self.is_active, self.owner_id, self.buyer_id)
+                db.execute("INSERT INTO ad (title, description, price, date, is_active, owner_id, buyer_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", values)
                 return self
             else:
-                values = (self.id, self.title, self.description, self.price, self.date, self.is_active, self.owner_id)
-                db.execute("REPLACE INTO ad (id, title, description, price, date, is_active, owner_id) VALUES (?, ?, ?, ?, ?, ?, ?)", values)
+                values = (self.id, self.title, self.description, self.price, self.date, self.is_active, self.owner_id, self.buyer_id)
+                db.execute("REPLACE INTO ad (id, title, description, price, date, is_active, owner_id, buyer_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", values)
                 return self
 
     @staticmethod
@@ -44,4 +45,3 @@ class Ad(object):
         result = None
         with SQLite() as db:
             result = db.execute("DELETE FROM ad WHERE owner_id = ?", (owner_id, ))
-    
