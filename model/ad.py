485a84999a1614fce1582a5deba9370f581b40e0
Maddie02 <madlensarkisian@gmail.com>
2020-01-22 16:38:14 +0200

    Added new methods
    Enable saving the ad and get all ads

--- model/ad.py
+++ model/ad.py
@@ -1,10 +1,30 @@
+from database import SQLite
+
 class Ad(object):
 
-    def __init__(self, ad_id, title, description, price, date, is_active, owner=None):
-        self.ad_id = ad_id
+    def __init__(self, id, title, description, price, date, is_active, owner_id):
+        self.id = id
         self.title = title
         self.description = description
         self.price = price
         self.date = date
         self.is_active = is_active
-        self.owner = owner
+        self.owner_id = owner_id
+
+    def to_dict(self):
+        ad_data = self.__dict__
+        return ad_data 
+   
+    def save(self):
+        with SQLite() as db:
+            values = (self.title, self.description, self.price, self.date, self.is_active, self.owner_id)
+            db.execute("INSERT INTO ad (title, description, price, date, is_active, owner_id) VALUES (?, ?, ?, ?, ?, ?)", values)
+            return self
+
+    @staticmethod
+    def get_all():
+        result = None
+        with SQLite() as db:
+            result = db.execute("SELECT * FROM ad").fetchall()
+            return [Ad(*row) for row in result]
+    
